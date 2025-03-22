5726175eec38bb913609630269e0bca081c92267
Lam Ngo <ngobaolam31122002@gmail.com>
2025-03-22 12:35:13 +0700

    Merge view with backend

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from pathlib import Path
 from PIL import Image, ImageTk
 from tkinter import filedialog
 from models import AudioPlayerModel
+from core import EqualizerService, PyAudioStreamWrapper
+from viewmodels.audio_player_viewmodel import AudioPlayerViewModel
 from views.graph_view import GraphView  # Import GraphView từ file của bạn
 from views.audio_player_view import AudioPlayerView  # Import GraphView từ file của bạn
 from views.equalizer_view import EqualizerView  # Import GraphView từ file của bạn
+
 
 class MainApplication(ttk.Window):
     def __init__(self):
@@ -21,12 +24,15 @@ class MainApplication(ttk.Window):
         self.rowconfigure(0, weight=1)     # Hàng trên - Player + Graph
         self.rowconfigure(1, weight=1)     # Hàng dưới - Equalizer
         
-        # Tạo frame bên trái để chứa AudioPlayerView
+        # Equalizer service
+        equalizer_svc = EqualizerService()
+        audio_model = AudioPlayerModel(PyAudioStreamWrapper, equalizer_svc)
+
+        # AudioPlayer
         left_frame = ttk.Frame(self)
         left_frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
-        
-        # Tạo AudioPlayerView
-        self.audio_player = AudioPlayerView(left_frame)
+        audio_player_viewmodel = AudioPlayerViewModel(audio_model, None)
+        self.audio_player_view = AudioPlayerView(left_frame, audio_player_viewmodel)
         
         # Tạo GraphView ở cột bên phải
         self.graph_view = GraphView(self, self.style)
@@ -37,18 +43,18 @@ class MainApplication(ttk.Window):
         self.equalizer.grid(row=1, column=0, columnspan=1, sticky="nsew", padx=10, pady=10)
         
         # Kết nối các thành phần
-        self.connect_components()
+        # self.connect_components()
         
-    def connect_components(self):
-        """Kết nối các thành phần với nhau"""
-        # Ví dụ: khi thay đổi equalizer, cập nhật các biểu đồ
-        self.equalizer.eq_status.trace_add("write", self.update_graphs)
+    # def connect_components(self):
+    #     """Kết nối các thành phần với nhau"""
+    #     # Ví dụ: khi thay đổi equalizer, cập nhật các biểu đồ
+    #     self.equalizer.eq_status.trace_add("write", self.update_graphs)
         
-    def update_graphs(self, *args):
-        """Cập nhật các biểu đồ khi equalizer thay đổi"""
-        if self.equalizer.eq_status.get():
-            # Nếu equalizer đang bật, cập nhật các biểu đồ
-            self.graph_view.update_all_graphs()
+    # def update_graphs(self, *args):
+    #     """Cập nhật các biểu đồ khi equalizer thay đổi"""
+    #     if self.equalizer.eq_status.get():
+    #         # Nếu equalizer đang bật, cập nhật các biểu đồ
+    #         self.graph_view.update_all_graphs()
 
 # Khởi tạo và chạy ứng dụng
 if __name__ == "__main__":
